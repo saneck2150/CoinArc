e5887b4110a28b87d0e22aa629de05fa6e8eef11
Oleksandr <saneck2150@gmail.com>
2025-03-22 23:44:30 +0100

    comments deleted

--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -6,12 +6,10 @@
 #include <QValueAxis>
 #include <QTimer>
 
-// Включаем пространство имён для QtCharts
 QT_BEGIN_NAMESPACE
 class QChart;
 QT_END_NAMESPACE
 
-// Прототипы используемых классов:
 class BinanceWebSocketApi;
 class BinanceRestApi;
 
@@ -23,10 +21,9 @@ public:
     ~MainWindow();
 
 private:
-    // Методы:
     void setupUi();
-    void initRestRequest();     // Можно сразу запросить REST для стартовой цены
-    void initWebSocketStream(); // Инициализируем WebSocket подписку
+    void initRestRequest();    
+    void initWebSocketStream(); 
 
 private slots:
     void onRestPriceReceived(const QString &symbol, double price);
@@ -34,17 +31,14 @@ private slots:
     void onWebSocketError(const QString &errorMsg);
 
 private:
-    // Графические элементы:
     QChart        *m_chart;
     QLineSeries   *m_series;
     QChartView    *m_chartView;
     QValueAxis              *m_axisX;
     QValueAxis              *m_axisY;
 
-    // Счётчик для оси X (имитация «временной» шкалы)
     int m_xCounter = 0;
 
-    // Binance API объекты:
     BinanceRestApi      *m_restApi;
     BinanceWebSocketApi *m_wsApi;
 };
